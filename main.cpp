78e054fe66132b12736597bb94ccee111ac2cb87
usuario <usuario@ejemplo.com>
2020-11-04 12:52:29 +0100

    Secuencia de 0 a 9

--- main.cpp
+++ main.cpp
@@ -1,8 +1,17 @@
 #include <iostream>
 
+void imprime_secuencia(){
+
+    for (int i = 0; i<10; i++)
+        std::cout<< i << " ";
+    std::cout<<std::endl;
+}
+
 int main(){
 
-    std::cout<< "\nHola mundo!"<<std::endl;
+    std::cout<< "\nHola mundo!\n"<<std::endl;
+
+    imprime_secuencia();
 
     std::cout<< "\nprograma finalizado correctamente\n"<<std::endl;
     
