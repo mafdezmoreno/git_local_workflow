75a579d746038c49dcfd319e981f141f21c69aa7
usuario <usuario@ejemplo.com>
2020-11-04 16:26:24 +0100

    Se ha requerido conocer la suma de la secuencia

--- main.cpp
+++ main.cpp
@@ -1,19 +1,26 @@
 #include <iostream>
 
-void imprime_secuencia(){
+int imprime_secuencia(){
 
-    for (int i = 0; i<10; i++)
+    int sum = 0;
+    for (int i = 0; i<10; i++){
         std::cout<< i << " ";
+        sum = sum + i;
+    }
     std::cout<<std::endl;
+
+    return sum;
 }
 
 int main(){
 
-    std::cout<< "\nHola mundo!\n"<<std::endl;
+    std::cout << "\nHola mundo!\n"<<std::endl;
 
-    imprime_secuencia();
+    std::cout   << "La suma de la secuencia: " << std::endl;
+    int temp = imprime_secuencia();
+    std::cout << "es: "<< temp <<std::endl;
 
-    std::cout<< "\nprograma finalizado correctamente\n"<<std::endl;
+    std::cout << "\nprograma finalizado correctamente\n" <<std::endl;
     
     return 0;
 }
