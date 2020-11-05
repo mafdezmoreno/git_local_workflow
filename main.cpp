2091932c13160f472a711fad70989df4054f4200
usuario <usuario@ejemplo.com>
2020-11-05 07:46:50 +0100

    Multiplicacion secuencia

--- main.cpp
+++ main.cpp
@@ -1,17 +1,41 @@
 #include <iostream>
+#include <vector>
 
-void imprime_secuencia(){
+std::vector<int> imprime_secuencia(){
 
-    for (int i = 0; i<10; i++)
+    std::vector<int> lista;
+    for (int i = 0; i<10; i++){
         std::cout<< i << " ";
+        lista.push_back(i);
+    }
     std::cout<<std::endl;
+
+    return lista;
+}
+
+int multiplica_secuencia(std::vector<int> v){
+
+    int producto = 1;
+    for(auto it = std::begin(v); it != std::end(v); it++) {
+        
+        if((*it)!=0){
+            producto = producto * (*it); 
+        }else{
+            std::cout<<"(Saltado: " << *it << ") "<<std::endl;
+        }
+
+    }    
+    return producto;
 }
 
 int main(){
 
     std::cout<< "\nHola mundo!\n"<<std::endl;
 
-    imprime_secuencia();
+    std::vector<int> lista = imprime_secuencia();
+
+    std::cout<<"El producto de la secuencia es: "
+             << multiplica_secuencia(lista)<<std::endl;
 
     std::cout<< "\nprograma finalizado correctamente\n"<<std::endl;
     
